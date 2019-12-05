a1c4b587694e5d6ef22a2e0ee73ad4399586cc60
Haesun-Pyeon <sme1357@naver.com>
2019-12-06 02:01:56 +0900

    dbimg

--- DB.py
+++ DB.py
@@ -30,7 +30,10 @@ if __name__ == '__main__':
             a[6]=a[6].replace(';','\n')
             openhour.append(a[6])
             saup.append(a[7])
-            img.append(a[8])
+            if img :
+                img.append('store/' + a[8])
+            else:
+                img.append(None)
 
     for i in range(len(name)):
         BookStore.objects.create(
@@ -42,7 +45,7 @@ if __name__ == '__main__':
         phone_number=phone_number[i],
         openhour=openhour[i],
         saup=saup[i],
-        img='store/' + img[i])
+        img=img[i])
         
     store = []
     title = []
